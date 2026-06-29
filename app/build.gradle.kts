1166ce1ddf135ba408dd9cd76dea5ce0d1295bc6
chrisu@hotmail.co.uk <13744596+chrisurwin@users.noreply.github.com>
2026-06-29 13:13:45 +0100

    Bump version to v0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.0.2"
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
