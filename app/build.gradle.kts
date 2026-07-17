ce2abee7c3f967ec13097ed40f16128e1c125dc2
chrisu@hotmail.co.uk <13744596+chrisurwin@users.noreply.github.com>
2026-07-17 12:55:14 +0100

    Bump version to v0.0.29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.chris.financeapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.0.28"
+        versionCode = 26
+        versionName = "0.0.29"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
